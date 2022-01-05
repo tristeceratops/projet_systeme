364ca0ef71c7fbe75ab6a7c3717ab7a327a175f7
Eric Woillard <76437817+tristeceratops@users.noreply.github.com>
2022-01-05 12:02:07 +0100

    infinte loop at game()

--- projet/serveurProj.c
+++ projet/serveurProj.c
@@ -8,9 +8,16 @@
 #include <time.h>
 #include <fcntl.h>
 
-char chaine[200] = "";
+#define MAXERREUR 7
+
+char mot[200] = "";
 int nbMots = 0;
 int rndmEntier = 0;
+char buffer[200] = "";
+char motCacher[200] = "";
+char lettre;
+
+
 
 
 int randomInt(int borninf,int bornsup)
@@ -21,36 +28,85 @@ int randomInt(int borninf,int bornsup)
     return n;
 }
 
-char * wordPicker(){
+void wordPicker(){
     FILE * fichier = fopen("listeMots","r");
      if ( fichier == NULL ) {
         printf( "Cannot open file %s\n");
         exit( 0 );
     }
-    while (fgets(chaine,30,fichier) != NULL)
+    while (fgets(mot,30,fichier) != NULL)
     {
         nbMots ++;
     }
     
     rndmEntier =  randomInt(1,nbMots);
     rewind(fichier);
-    while (fgets(chaine,30,fichier) != NULL && nbMots != rndmEntier)
+    while (fgets(mot,30,fichier) != NULL && nbMots != rndmEntier)
     {
             nbMots --;
     }
     
     fclose(fichier);
-    return chaine;
+}
+
+void afficherAscii(char * pathname){
+    FILE * fichier = fopen(pathname,"r");
+     if ( fichier == NULL ) {
+        printf( "Cannot open file %s\n");
+        exit( 0 );
+    }
+    while (fgets(buffer,30,fichier) != NULL)
+    {
+        printf(buffer);
+    }
+    fclose(fichier);
+}
+
+void hiddenWord(char * word){
+    int taille = strlen(word);
+    for (int i = 1; i<taille;i++)
+    {
+        strcat(motCacher,"-");
+    }
+    strcat(motCacher,"\n");
 }
 
 
 
+void verifLettre(char lettre){
+    int taille = strlen(mot);
+    for (int i = 1; i<taille;i++)
+    {
+        if(mot[i-1] == lettre)
+        {
+            motCacher[i-1] == lettre;
+        }
+    }
+    
+}
 
+void demanderLettre(){
+    printf("Saisissez une lettre\n");
+    scanf(" %c",lettre);
+    printf("%c",lettre);
+    verifLettre(lettre);
+}
+
+void game(){
+    wordPicker();
+    hiddenWord(mot);
+    int erreur = 0;
+    int trouve = 0;
+    do 
+    {
+        printf(motCacher);
+        printf("%c",mot[0]);
+        demanderLettre();
+    } while (erreur <= MAXERREUR && trouve == 0);
+}
 
 
 int main(int argc, char const *argv[]) {
-    char * chariot;
-   chariot= wordPicker();
-   printf(chariot);
-    return EXIT_SUCCESS;
+  game();
+   return EXIT_SUCCESS;
 }
