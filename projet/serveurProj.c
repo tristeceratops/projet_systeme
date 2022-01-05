759e4c79a7d9eb92d28ab2181aec547a63d0a779
Cliffy57 <hugodip@orange.fr>
2022-01-05 11:27:40 +0100

    cli et serv , le serveur peut avoir 10 connexion simul, le client se connecte, son ip est affiche cote serv , il rentre son pseudo qui s'affiche cote serv, la connexion client se coupe et le serv continue de tourner

--- projet/serveurProj.c
+++ projet/serveurProj.c
@@ -1,153 +1,94 @@
-#include <netinet/in.h>
 #include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <unistd.h>
-#include <time.h>
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
 
-#include <fcntl.h>
+// Function designed for chat between client and server.
+void func(int connfd)
+{
+    char buff[MAX];
+    int n;
+    // infinite loop for chat
+    for (;;) {
+        bzero(buff, MAX);
 
+        // read the message from client and copy it in buffer
+        read(connfd, buff, sizeof(buff));
+        // print buffer which contains the client contents
+        printf("From client: %s\t To client : ", buff);
+        bzero(buff, MAX);
+        n = 0;
+        // copy server message in the buffer
+        while ((buff[n++] = getchar()) != '\n')
+            ;
 
-#define PORT 6000
-#define MAX_BUFFER 1000
-#define MAX_LENGTH_WORD 30
+        // and send that buffer to client
+        write(connfd, buff, sizeof(buff));
 
-int fichier1;
-const char *cool;
-
-const char *readLine(FILE *file) {
-
-    if (file == NULL) {
-        printf("Error: file pointer is null.");
-        exit(1);
-    }
-
-    int maximumLineLength = 128;
-    char *lineBuffer = (char *)malloc(sizeof(char) * maximumLineLength);
-
-    if (lineBuffer == NULL) {
-        printf("Error allocating memory for line buffer.");
-        exit(1);
-    }
-
-    char ch = getc(file);
-    int count = 0;
-
-    while ((ch != '\n') && (ch != EOF)) {
-        if (count == maximumLineLength) {
-            maximumLineLength += 128;
-            lineBuffer = realloc(lineBuffer, maximumLineLength);
-            if (lineBuffer == NULL) {
-                printf("Error reallocating space for line buffer.");
-                exit(1);
-            }
+        // if msg contains "Exit" then server exit and chat ended.
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
         }
-        lineBuffer[count] = ch;
-        count++;
-
-        ch = getc(file);
-    }
-
-    lineBuffer[count] = '\0';
-    char line[count + 1];
-    strncpy(line, lineBuffer, (count + 1));
-    free(lineBuffer);
-    const char *constLine = line;
-    return constLine;
-}
-
-int lecturelistMots(){
-        if ((fichier1 = open("listeMots.txt", O_RDONLY)) < 0) {
-        printf("Erreur ouverture fichier 1\n");
-        return -1;
-    }
-    for (int i = 0; i < sizeof(fichier1); i++){
-        printf("pouet"); 
     }
 }
 
-int randomInt(int minimum_number,int max_number){
-    return rand() % (max_number + 1 - minimum_number) + minimum_number;
-}
+// Driver function
+int main()
+{
+    int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
 
-int ouvrirUneSocketAttente() {
-    int socketTemp;
-    int longueurAdresse;
-    struct sockaddr_in coordonneesServeur;
-
-    socketTemp = socket(PF_INET, SOCK_STREAM, 0); //Création d'une socket avec paramètre PF_INET : domaine IPV4 pour serveur, SOCK_STREAM : TCP, 0 : protocole
-
-    if (socketTemp < 0) {//Détection erreur création socket
-        printf("socket incorrecte\n");
-        exit(EXIT_FAILURE);
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
     }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
 
-    // On prépare l’adresse d’attachement locale
-    longueurAdresse = sizeof(struct sockaddr_in);
-    memset(&coordonneesServeur, 0x00, longueurAdresse); //memset pour initialiser la zone mémoire avec une valeur, ici 0x00
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
 
-    coordonneesServeur.sin_family = PF_INET; //set up la famille d'adresse à PF_INET (similaire à AF_INET)
-    // toutes les interfaces locales disponibles
-    coordonneesServeur.sin_addr.s_addr = htonl(INADDR_ANY); //Création de l'adresse IPV4
-    // toutes les interfaces locales disponibles
-    coordonneesServeur.sin_port = htons(PORT); //Affection au port 6000
-
-    if (bind(socketTemp, (struct sockaddr *) &coordonneesServeur, sizeof(coordonneesServeur)) == -1) {
-        printf("erreur de bind\n"); //Erreur de liaison entre la socket et le serveur
-        exit(EXIT_FAILURE);
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
     }
+    else
+        printf("Socket successfully binded..\n");
 
-    if (listen(socketTemp, 5) == -1) {
-        printf("erreur de listen\n"); //Erreur de mise en attente de la socket
-        exit(EXIT_FAILURE);
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
     }
+    else
+        printf("Server listening..\n");
+    len = sizeof(cli);
 
-    printf("En attente de connexion...\n");
-
-    return socketTemp;
-}
-
-
-int main(int argc, char const *argv[]) {
-    int fdSocketAttente;
-    int fdSocketCommunication;
-    struct sockaddr_in coordonneesAppelant;
-    char tampon[MAX_BUFFER];
-    int nbRecu;
-    const char * tablo = readLine(fichier1);
-
-    fdSocketAttente = ouvrirUneSocketAttente();
-
-    socklen_t tailleCoord = sizeof(coordonneesAppelant);
-
-    if ((fdSocketCommunication = accept(fdSocketAttente, (struct sockaddr *) &coordonneesAppelant,
-                                        &tailleCoord)) == -1) {
-        printf("erreur de accept\n"); //Erreur de connexion
-        printf(tablo[randomInt(1,10)]);
-        exit(-1);
+    // Accept the data packet from client and verification
+    connfd = accept(sockfd, (SA*)&cli, &len);
+    if (connfd < 0) {
+        printf("server accept failed...\n");
+        exit(0);
     }
+    else
+        printf("server accept the client...\n");x
 
-    printf("Client connecté\n");
+    // Function for chatting between client and server
+    func(connfd);
 
-    // on attend le message du client
-    // la fonction recv est bloquante
-    nbRecu = recv(fdSocketCommunication, tampon, MAX_BUFFER, 0); // on attend la réponse du client
-
-    if (nbRecu > 0) { //Si on reçoit la réponse du client
-        tampon[nbRecu] = 0;
-        printf("Recu : %s\n", tampon); //On affiche le message du client
-        printf(tablo[randomInt(1,10)]);
-    }
-
-    printf("Envoi du message au client.\n");
-    strcpy(tampon, "Message renvoyé par le serveur vers le client !"); //concaténation d'une chaine de caractère
-    // on envoie le message au client
-    send(fdSocketCommunication, tampon, strlen(tampon), 0);  //Envoi d'un bloc de donnée sur la socket
-
-    close(fdSocketCommunication); //Cloture le canal de communication, fermeture de la connexion TCP
-    close(fdSocketAttente);
-
-    return EXIT_SUCCESS;
+    // After chatting close the socket
+    close(sockfd);
 }
