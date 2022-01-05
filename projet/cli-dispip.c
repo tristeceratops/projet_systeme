e2b13699e9ac872ee5c6d80fa4d759bdfc212616
Cliffy57 <hugodip@orange.fr>
2022-01-05 11:28:00 +0100

    cli et serv , le serveur peut avoir 10 connexion simul, le client se connecte, son ip est affiche cote serv , il rentre son pseudo qui s'affiche cote serv, la connexion client se coupe et le serv continue de tourner

---
+++ projet/cli-dispip.c
@@ -0,0 +1,86 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 6000
+#define MAX_BUFFER 1000
+
+const char *EXIT = "exit";
+
+
+void lireMessage(char tampon[]) {
+    printf("Saisir votre peuso :\n");
+    fgets(tampon, MAX_BUFFER, stdin);
+    strtok(tampon, "\n");
+}
+
+int testQuitter(char tampon[]) {
+    return strcmp(tampon, EXIT) == 0;
+}
+
+
+int main(int argc , char const *argv[]) {
+    int fdSocket;
+    int nbRecu;
+    struct sockaddr_in coordonneesServeur;
+    int longueurAdresse;
+    char tampon[MAX_BUFFER];
+
+    fdSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (fdSocket < 0) {
+        printf("socket incorrecte\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // On prépare les coordonnées du serveur
+    longueurAdresse = sizeof(struct sockaddr_in);
+    memset(&coordonneesServeur, 0x00, longueurAdresse);
+
+    coordonneesServeur.sin_family = PF_INET;
+    // adresse du serveur
+    inet_aton("127.0.0.1", &coordonneesServeur.sin_addr);
+    // toutes les interfaces locales disponibles
+    coordonneesServeur.sin_port = htons(PORT);
+
+    if (connect(fdSocket, (struct sockaddr *) &coordonneesServeur, sizeof(coordonneesServeur)) == -1) {
+        printf("connexion impossible\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("connexion ok\n");
+
+    while (1) {
+
+        lireMessage(tampon);
+
+        if (testQuitter(tampon)) {
+            send(fdSocket, tampon, strlen(tampon), 0);
+            break; // on quitte la boucle
+        }
+
+        // on envoie le message au serveur
+        send(fdSocket, tampon, strlen(tampon), 0);
+        break;
+//        // on attend la réponse du serveur
+//        nbRecu = recv(fdSocket, tampon, MAX_BUFFER, 0);
+//
+//        if (nbRecu > 0) {
+//            tampon[nbRecu] = 0;
+//            printf("Recu : %s\n", tampon);
+//
+//            if (testQuitter(tampon)) {
+//                break; // on quitte la boucle
+//            }
+//        }
+    }
+
+    close(fdSocket);
+
+    return EXIT_SUCCESS;
+}
---
+++ projet/serv-dispip.c
@@ -0,0 +1,127 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define PORT 6000
+#define MAX_BUFFER 1000
+#define MAX_CLIENTS 10
+
+const char *EXIT = "exit";
+
+
+void lireMessage(char tampon[]) {
+    printf("Saisir un message à envoyer :\n");
+    fgets(tampon, MAX_BUFFER, stdin);
+    strtok(tampon, "\n");
+}
+
+int testQuitter(char tampon[]) {
+    return strcmp(tampon, EXIT) == 0;
+}
+
+
+int main(int argc, char const *argv[]) {
+    int fdSocketAttente;
+    int fdSocketCommunication;
+    struct sockaddr_in coordonneesServeur;
+    struct sockaddr_in coordonneesAppelant;
+    char tampon[MAX_BUFFER];
+    int nbRecu;
+    int longueurAdresse;
+    int pid;
+
+    fdSocketAttente = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (fdSocketAttente < 0) {
+        printf("socket incorrecte\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // On prépare l’adresse d’attachement locale
+    longueurAdresse = sizeof(struct sockaddr_in);
+    memset(&coordonneesServeur, 0x00, longueurAdresse);
+
+    coordonneesServeur.sin_family = PF_INET;
+    // toutes les interfaces locales disponibles
+    coordonneesServeur.sin_addr.s_addr = htonl(INADDR_ANY);
+    // toutes les interfaces locales disponibles
+    coordonneesServeur.sin_port = htons(PORT);
+
+    if (bind(fdSocketAttente, (struct sockaddr *) &coordonneesServeur, sizeof(coordonneesServeur)) == -1) {
+        printf("erreur de bind\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(fdSocketAttente, 5) == -1) {
+        printf("erreur de listen\n");
+        exit(EXIT_FAILURE);
+    }
+
+    socklen_t tailleCoord = sizeof(coordonneesAppelant);
+
+    int nbClients = 0;
+
+    while (nbClients < MAX_CLIENTS) {
+        if ((fdSocketCommunication = accept(fdSocketAttente, (struct sockaddr *) &coordonneesAppelant,
+                                            &tailleCoord)) == -1) {
+            printf("erreur de accept\n");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Client connecté - %s:%d\n",
+               inet_ntoa(coordonneesAppelant.sin_addr),
+               ntohs(coordonneesAppelant.sin_port));
+
+        if ((pid = fork()) == 0) {
+            close(fdSocketAttente);
+
+            while (1) {
+                // on attend le message du client
+                // la fonction recv est bloquante
+                nbRecu = recv(fdSocketCommunication, tampon, MAX_BUFFER, 0);
+
+                if (nbRecu > 0) {
+                    tampon[nbRecu] = 0;
+                    printf("Pseudo de %s:%d : %s\n",
+                           inet_ntoa(coordonneesAppelant.sin_addr),
+                           ntohs(coordonneesAppelant.sin_port),
+                           tampon);
+
+                    if (testQuitter(tampon)) {
+                        break; // on quitte la boucle
+                    }
+                }
+
+//                lireMessage(tampon);
+
+                if (testQuitter(tampon)) {
+                    send(fdSocketCommunication, tampon, strlen(tampon), 0);
+                    break; // on quitte la boucle
+                }
+
+                // on envoie le message au client
+                send(fdSocketCommunication, tampon, strlen(tampon), 0);
+            }
+
+            exit(EXIT_SUCCESS);
+        }
+
+        nbClients++;
+    }
+
+    close(fdSocketCommunication);
+    close(fdSocketAttente);
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        wait(NULL);
+    }
+
+    printf("Fin du programme.\n");
+    return EXIT_SUCCESS;
+}
